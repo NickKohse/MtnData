91e895d16c6ce82179ace64ea1f47c040d6dc8fe
NickKohse <nick.kohse@gmail.com>
2018-09-08 19:21:03 -0600

    fixes for previous commit

--- MtnData/MtnData/Controllers/LocationController.cs
+++ MtnData/MtnData/Controllers/LocationController.cs
@@ -29,7 +29,7 @@ namespace MtnData.Controllers
             int physical = 0;
             int technical = 0;
             int final = 0;
-            bool ver = false;
+            //bool ver = false; **currently recieves this as null, possibly due to the diabled attribute in the html
             Coordinate start = null;
             Coordinate end = null;
             try
@@ -39,16 +39,16 @@ namespace MtnData.Controllers
                 physical = int.Parse(pDiff);
                 technical = int.Parse(TDiff);
                 final = int.Parse(peakEv);
-                ver = bool.Parse(verified);
+                //ver = bool.Parse(verified);
                 start = new Coordinate(double.Parse(startN), double.Parse(startW));
                 end = new Coordinate(double.Parse(endN), double.Parse(endW));
 
             }
             catch(Exception ex)
             {
-                Utilities.ExceptionLogger("Unparseable input in NewLoc function. Exception message" + ex.Message);
+                Utilities.ExceptionLogger("Unparseable input in NewLoc function. Exception message: " + ex.Message);
             }
-            Location newLoc = new Location(name, region, ev_gain, dist, start, end, physical, technical, final, ver, description);
+            Location newLoc = new Location(name, region, ev_gain, dist, start, end, physical, technical, final, false /*for now a new location is always unverified*/, description);
             LocationConnect lc = new LocationConnect();
             Message response = lc.AddLocation(newLoc);
 
@@ -60,6 +60,7 @@ namespace MtnData.Controllers
             else
             {
                 Utilities.EventLogger("Couldn't create a new location named: " + name + " Resulting message: " + response.GetText(), Globals.LOG_LEVELS.Error);
+                ViewBag.createdlocationResponse = response.GetText();
                 return View("NewLocation");
             }
         }
